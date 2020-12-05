c374bd06855eb8888b9da22d42ece6047c40aef5
jbrajkovic <brajkovic.josip96@gmail.com>
2020-12-06 00:48:51 +0100

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <picture>
-        <source media='(min-width:768px)' srcset={process.env.PUBLIC_URL + '/images/bg-header-desktop.svg'} />
+        <source media='(min-width:768px)' srcSet={process.env.PUBLIC_URL + '/images/bg-header-desktop.svg'} />
         <img
           src={process.env.PUBLIC_URL + '/images/bg-header-mobile.svg'}
           alt='Header'
