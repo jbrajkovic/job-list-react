6b09fb6f410595302e4a35c2fec448a71bf6310e
jbrajkovic <brajkovic.josip96@gmail.com>
2020-12-08 18:37:37 +0100

    code optimization

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import { useContext } from 'react'
 import { AppContext } from './contexts/AppContext'
 import loadable from '@loadable/component'
 
-/* import FilterBox from './components/FilterBox'
-import JobCard from './components/JobCard'
-import Loader from './components/Loader' */
-//import Header from './components/Header'
+import FilterBox from './components/FilterBox'
+import Loader from './components/Loader'
+import Header from './components/Header'
 
-const FilterBox = loadable(() => import('./components/FilterBox'))
 const JobCard = loadable(() => import('./components/JobCard'))
-const Loader = loadable(() => import('./components/Loader'))
-const Header = loadable(() => import('./components/Header'))
 
 function App() {
   const { filteredJobs, loading, activeTags } = useContext(AppContext)
